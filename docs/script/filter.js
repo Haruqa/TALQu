0106a253dd346ef9eafcc3d808fd55288e3ec053
Haruqa <52907049+Haruqa@users.noreply.github.com>
2022-01-10 11:23:07 +0900

    Add teto, change filter

--- docs/script/filter.js
+++ docs/script/filter.js
@@ -13,13 +13,52 @@ $(function(){
             }
         });
 
+        //and filter
         $('[name="filterAnd"]').each(function(index, element){
-            if($(element).prop('checked')){
-                $('.no' + $(element).val()).hide();
-            }else{
-                $('.' + $(element).val()).hide();
-            }
+            //all item
+            $(".item").each(function(i, item){
+                if($(element).prop('checked')){
+                    if(item.classList.contains($(element).val())){
+                        $(item).show();
+                    }else{
+                        $(item).hide();
+                    }
+                }
+                else{
+                    if(item.classList.contains($(element).val())){
+                        $(item).hide();
+                    }
+                }
+                
+            });
         });
+
+        var addFilterFlg = false;
+        //add filter チェックされたらそれを持たないアイテムを隠す
+        $('[name="filterAdd"]').each(function(index, element){
+            //all item
+            $(".item").each(function(i, item){
+                if($(element).prop('checked')){
+                    if(item.classList.contains($(element).val())){
+                        $(item).show();
+                    }else{
+                        $(item).hide();
+                    }
+                    addFilterFlg = true;
+                }
+            });
+        });
+
+        if(addFilterFlg){
+            //ORフィルタのhide
+            $('[name="filterOr"]').each(function(index, element){
+                if($(element).prop('checked')){
+
+                }else{
+                    $('.' + $(element).val()).hide();
+                }
+            });
+        }
         
     }
     $(document).ready(function(){
@@ -29,6 +68,9 @@ $(function(){
         executeFilter();
     });
     $('[name="filterAnd"]').change(function(){
+        executeFilter();
+    });
+    $('[name="filterAdd"]').change(function(){
         executeFilter();
     });
 });
