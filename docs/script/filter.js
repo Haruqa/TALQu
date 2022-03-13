b7507297d4ec9bc8389b8aacfa5e93a0fb505d17
Haruqa <52907049+Haruqa@users.noreply.github.com>
2022-03-14 01:10:52 +0900

    Add models, change filter
    アイコンとか色々追加

--- docs/script/filter.js
+++ docs/script/filter.js
@@ -1,9 +1,10 @@
 $(function(){
-    function executeFilter(){
+    function executeFilterOr(){
         //all show 
         $('[name="filterOr"]').each(function(index, element){
             $('.' + $(element).val()).show();
         });
+
         //hide
         $('[name="filterOr"]').each(function(index, element){
             if($(element).prop('checked')){
@@ -12,65 +13,120 @@ $(function(){
                 $('.' + $(element).val()).hide();
             }
         });
+    }
+    function executeFilterKN(){
+        //all hide 
+        $('[name="filterOr"]').each(function(index, element){
+            $('.' + $(element).val()).hide();
+        });
 
-        //and filter
-        $('[name="filterAnd"]').each(function(index, element){
+        //add filter
+        var itemList = [];
+        var checked = false;
+        var checked2 = false;
+        $('[name="filterKoebetu"]').each(function(index, element){
             //all item
             $(".item").each(function(i, item){
                 if($(element).prop('checked')){
                     if(item.classList.contains($(element).val())){
-                        $(item).show();
-                    }else{
-                        $(item).hide();
+                        //$(item).show();
+                        if(!itemList.includes(item)){
+                            itemList.push(item);
+                        }
                     }
+                    checked = true;
                 }
-                else{
-                    if(item.classList.contains($(element).val())){
-                        $(item).hide();
-                    }
-                }
-                
             });
         });
+        
+        if(checked){
+            $('[name="filterNenre"]').each(function(index, element){
+                itemList.forEach(function(item){
+                    if($(element).prop('checked')){
+                        if(item.classList.contains($(element).val())){
+                            $(item).show();
+                        }
+                        checked2 = true;
+                    }
+                });
+            });
 
-        var addFilterFlg = false;
-        //add filter チェックされたらそれを持たないアイテムを隠す
+        }
+        else{
+            $('[name="filterNenre"]').each(function(index, element){
+                //all item
+                $(".item").each(function(i, item){
+                    if($(element).prop('checked')){
+                        if(item.classList.contains($(element).val())){
+                            $(item).show();
+                        }
+                        checked2 = true;
+                    }
+                });
+            });
+        }
+        if(!checked2){
+            itemList.forEach(function(item){
+                $(item).show();
+            });
+        }
+        
+        if(!checked && !checked2){
+            executeFilterOr();
+            enabledOtherFilter();
+        }else{
+            disabledOtherFilter();
+        }
+    }
+    function executeFilterAdd(){
+        //all hide 
+        $('[name="filterOr"]').each(function(index, element){
+            $('.' + $(element).val()).hide();
+        });
+
+        //add filter
+        var checked = false;
         $('[name="filterAdd"]').each(function(index, element){
             //all item
             $(".item").each(function(i, item){
                 if($(element).prop('checked')){
                     if(item.classList.contains($(element).val())){
                         $(item).show();
-                    }else{
-                        $(item).hide();
                     }
-                    addFilterFlg = true;
+                    checked = true;
                 }
             });
         });
 
-        if(addFilterFlg){
-            //ORフィルタのhide
-            $('[name="filterOr"]').each(function(index, element){
-                if($(element).prop('checked')){
-
-                }else{
-                    $('.' + $(element).val()).hide();
-                }
-            });
+        if(!checked){
+            executeFilterOr();
+            executeFilterKN();
         }
-        
+    }
+    function disabledOtherFilter(){
+        $('[name="filterAdd"]').each(function(index, element){
+            element.checked = false;
+            element.disabled = true;
+        });
+    }
+    function enabledOtherFilter(){
+        $('[name="filterAdd"]').each(function(index, element){
+            element.disabled = false;
+        });
     }
     $(document).ready(function(){
-        executeFilter()
+        executeFilterOr()
     });
     $('[name="filterOr"]').change(function(){
-        executeFilter();
+        executeFilterOr();
     });
-    $('[name="filterAnd"]').change(function(){
-        executeFilter();
+    $('[name="filterKoebetu"]').change(function(){
+        executeFilterKN();
+    });
+    $('[name="filterNenre"]').change(function(){
+        executeFilterKN();
     });
     $('[name="filterAdd"]').change(function(){
-        executeFilter();
+        executeFilterAdd();
     });
 });
